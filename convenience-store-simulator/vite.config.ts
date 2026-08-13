9f35af06da78f5e86d47fc0587c731f3e6c9dbfe
xh91900 <xh91900@126.com>
2026-08-13 13:14:11 +0800

    deploy to vercel

--- convenience-store-simulator/vite.config.ts
+++ convenience-store-simulator/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//     base: '/CDXSS/',
+//     plugins: [react()]
+// })
+
 export default defineConfig({
-    base: '/CDXSS/',
-    plugins: [react()]
+    plugins: [react()],
+    base: './' // 静态相对路径，vercel根域名部署用这个；不要写子目录路径
 })
