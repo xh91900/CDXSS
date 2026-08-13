bd230800d2490f42d251abeaf28c1a4140576bda
xh91900 <xh91900@126.com>
2026-08-13 13:27:19 +0800

    deploy to vercel

--- convenience-store-simulator/vite.config.js
+++ convenience-store-simulator/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// export default defineConfig({
+//     base: '/CDXSS/',
+//     plugins: [react()]
+// })
 export default defineConfig({
-    base: '/CDXSS/',
-    plugins: [react()]
-});
+    plugins: [react()],
+    base: './'
+})
--- convenience-store-simulator/vite.config.ts
+++ convenience-store-simulator/vite.config.ts
@@ -8,5 +8,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: './' // 静态相对路径，vercel根域名部署用这个；不要写子目录路径
+    base: './'
 })
